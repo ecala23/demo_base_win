4f6925b857f62e9cdffd95b690cceacacf223d93
Eric Calabretta <ericcalabretta@users.noreply.github.com>
2017-08-01 15:19:32 -0500

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,7 @@
 #change version number - next chef client run will upgrade/downgrade client
 default['omnibus_updater']['version'] = '12.20.3'
 #Configures audit-coobook - to run profiles and report back to automate
-default['audit']['fetcher'] = 'chef-automate'
-default['audit']['reporter'] = 'chef-automate'
+default['audit']['reporter'] = 'chef-server-automate'
 default['audit']['profiles'] = [
   {
     name: 'workstation-1/windows-baseline',
